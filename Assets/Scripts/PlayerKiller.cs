c8976de87b901fd6e3d87152bffb488b62d10f53
Joshua Lowe <joshua220104@icloud.com>
2026-02-11 11:47:57 +0000

    updating how players are killed

--- Assets/Scripts/PlayerKiller.cs
+++ Assets/Scripts/PlayerKiller.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerKiller : MonoBehaviour
 {
     void OnTriggerEnter(Collider collider)
     {
-        PlayerHealth health = collider.gameObject.GetComponent<PlayerHealth>();
-        if (health)
+        
+        if (NetworkManager.Singleton.IsServer)
         {
-            health.Health.Value = 0;
+            PlayerHealth health = collider.gameObject.GetComponent<PlayerHealth>();
+            if (health)
+            {
+                health.Health.Value = 0;
+            }
         }
     }
 }
